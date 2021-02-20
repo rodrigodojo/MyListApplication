ad613790c40956af4456c1294b00521659d1b412
rodrigodojo <rodrigodoj@gmail.com>
2021-02-20 13:29:09 -0300

    criando as classes MeuAdaptador, ViewPersonagem e DadosPersonagem na classe MainActivity.java

--- app/src/main/java/com/dojo/mylistapplication/MainActivity.java
+++ app/src/main/java/com/dojo/mylistapplication/MainActivity.java
@@ -1,12 +1,18 @@
 package com.dojo.mylistapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,5 +37,58 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,""+listaNomes[position],Toast.LENGTH_SHORT).show();
             }
         });
+    }
+}
+
+class ViewPersonagem{
+    ImageView icone;
+    TextView titulo;
+    TextView descricao;
+}
+
+class DadosPersonagem{
+    private int icone;
+    private String titulo;
+    private String descricao;
+
+    public DadosPersonagem(int icone, String nome, String descricao) {
+        this.icone = icone;
+        this.titulo = nome;
+        this.descricao = descricao;
+    }
+
+    public int getIcone() {
+        return icone;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
+
+class MeuAdaptador extends ArrayAdapter{
+
+    public MeuAdaptador(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+
+    @Override
+    public void add(@Nullable Object object) {
+        super.add(object);
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
     }
 }
